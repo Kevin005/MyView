8389a509dd0cdecd37a8bad49c83129fa67ee3c3
Kevin <18717825632@163.com>
2017-02-16 21:44:58 +0800

    add two file

---
+++ app/src/main/java/com/view/kevin/myview/activity/view/LineEditText.java
@@ -0,0 +1,33 @@
+package com.xsfuture.xsfuture2.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+import com.xsfuture.xsfuture2.R;
+import com.xsfuture.xsfuture2.utils.DPIUtils;
+
+public class LineEditText extends EditText {
+    private Paint mPaint;
+
+    /**
+     * @param context
+     * @param attrs
+     */
+    public LineEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mPaint = new Paint();
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(DPIUtils.dip2px(context, 1));
+        mPaint.setColor(getResources().getColor(R.color.main_color));
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        //画底线
+        canvas.drawLine(0, this.getHeight() - 1, this.getWidth() - 1, this.getHeight() - 1, mPaint);
+    }
+}
