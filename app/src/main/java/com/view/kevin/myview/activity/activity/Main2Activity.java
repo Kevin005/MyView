7816784ef547e05852093e3737e301ef76e50cd0
kevin <18717825632@163.com>
2017-02-20 18:52:11 +0800

    add a view

--- app/src/main/java/com/view/kevin/myview/activity/activity/Main2Activity.java
+++ app/src/main/java/com/view/kevin/myview/activity/activity/Main2Activity.java
@@ -12,6 +12,7 @@ import android.widget.AdapterView;
 import android.widget.Gallery;
 import android.widget.Toast;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.view.kevin.myview.R;
 import com.view.kevin.myview.activity.adapter.GalleryItemAdapter;
 import com.view.kevin.myview.activity.view.GalleryCoverFlow;
@@ -22,29 +23,16 @@ import java.util.ArrayList;
  * this is add
  */
 public class Main2Activity extends AppCompatActivity {
-    private GalleryCoverFlow gallery;
+    private LottieAnimationView animationView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        gallery = (GalleryCoverFlow) findViewById(R.id.gallery);
-        gallery.setUnselectedScale(0.75f);
-        gallery.setUnselectedAlpha(0.75f);
-        gallery.setSpacing(20);
-        gallery.setGravity(Gravity.CENTER_VERTICAL);
-        //设置图片适配器
-        gallery.setAdapter(new GalleryItemAdapter(this));
-        gallery.setSelection(2);
-        //设置监听器
-        gallery.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-                Toast.makeText(Main2Activity.this, "点击了第" + arg2 + "张图片", Toast.LENGTH_LONG).show();
-            }
-        });
+        animationView = (LottieAnimationView) findViewById(R.id.animation_view);
+        animationView.setAnimation("hello-world.json");
+        animationView.loop(true);
 
     }
-
 }
 
