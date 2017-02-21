0e4ec05cf0c73c786dc521725217400ed47f858f
kevin <18717825632@163.com>
2017-02-21 13:38:32 +0800

    add lottieAnimationActivity

--- app/src/main/java/com/view/kevin/myview/activity/activity/Main2Activity.java
+++ app/src/main/java/com/view/kevin/myview/activity/activity/Main2Activity.java
@@ -1,23 +1,13 @@
 package com.view.kevin.myview.activity.activity;
 
 import android.os.Bundle;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.PagerTitleStrip;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Gallery;
-import android.widget.Toast;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.view.kevin.myview.R;
-import com.view.kevin.myview.activity.adapter.GalleryItemAdapter;
-import com.view.kevin.myview.activity.view.GalleryCoverFlow;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 /**
  * this is add
@@ -29,10 +19,22 @@ public class Main2Activity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        animationView = (LottieAnimationView) findViewById(R.id.animation_view);
-        animationView.setAnimation("hello-world.json");
-        animationView.loop(true);
-
     }
+
+    public String getFromAssets(String fileName){
+        try {
+            InputStreamReader inputReader = new InputStreamReader( getResources().getAssets().open(fileName) );
+            BufferedReader bufReader = new BufferedReader(inputReader);
+            String line="";
+            String Result="";
+            while((line = bufReader.readLine()) != null)
+                Result += line;
+            return Result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
 }
 
---
+++ app/src/main/java/com/view/kevin/myview/activity/view/activity/LottieAnimationActivity.java
@@ -0,0 +1,22 @@
+package com.view.kevin.myview.activity.view.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.view.kevin.myview.R;
+
+public class LottieAnimationActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_lottie_animation);
+
+    }
+
+}
